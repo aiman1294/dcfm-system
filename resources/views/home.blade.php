430f46d56a650db73f0d413a6f3ca357d4043528
aiman1294 <13aimanmk@gmail.com>
2026-04-30 13:02:09 +0530

    added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>HOME</title>
+</head>
+<body>
+    <div>
+        <h1>HOME</h1>
+        <p>Welcome to the home page!</p>
+    </div>
+</body>
+</html>
