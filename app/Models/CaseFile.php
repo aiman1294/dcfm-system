7cbdb6548e0075f4b19a1947024448941164f670
aiman1294 <13aimanmk@gmail.com>
2026-05-03 16:32:12 +0530

    Update CaseFile fillable fields to support role-based updates

--- app/Models/CaseFile.php
+++ app/Models/CaseFile.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CaseFile extends Model
 {
     //
-    protected $fillable = ['case_title', 'case_description', 'case_priority', 'case_status', 'user_id','judge_id',];
+    protected $fillable = ['case_title', 'case_description', 'case_priority', 'case_status', 'user_id','judge_id'];
 
     public function judge(){
         return $this->belongsTo(User::class, 'judge_id');
